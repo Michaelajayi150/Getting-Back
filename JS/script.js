5ef6990d9f133f43b383be0c49350648f99aebbf
Ajayi Michael <ajayimichael150@gmail.com>
2020-12-24 00:57:28 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,28 @@ function mobileToggleOn() {
     sub.classList.add('menu-toggle');
   }
 }
+
+var frame,
+    draw = function() {
+      var act = document.querySelector('.coloring');
+      if (act.style.opacity == 0) {
+        act.style.opacity = 1;
+      } else {
+        act.style.opacity = 0;
+        clearInterval(frame);
+      }
+    };
+
+function notifyOn(inputEmail) {
+  var mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (inputEmail.value.match(mailFormat)) {
+    frame = setInterval(function() {
+        draw();
+      }, 1500);
+    return true;
+  } else {
+    alert("You have entered an invalid email");
+    document.form1.text1.focus();
+    return false;
+  }
+}
